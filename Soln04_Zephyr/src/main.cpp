cfac86e64ba8bf61c21a8e7b8d1af797309f0793
Akshay Narahari Kulkarni <akshaynkulkarni@gmail.com>
2023-06-23 23:48:24 +0200

    Soln04_Zephyr: Memory Management Demo
    This patch is inspired from challenge at  www.digiKey.in
    (https://www.digikey.in/en/maker/projects/introduction-to-rtos-
    solution-to-part-4-memory-management/6d4dfcaa1ff84f57a2098da8e6401d9c)
    
    Using FreeRTOS, create two separate tasks. One listens for input over UART
    (from the Serial Monitor). Upon receiving a newline character (‘\n’), the
    task allocates a new section of heap memory (using pvPortMalloc()) and stores
    the string up to the newline character in that section of heap. It then notifies
    the second task that a message is ready.
    
    The second task waits for notification from the first task. When it receives that
    notification, it prints the message in heap memory to the Serial Monitor. Finally,
    it deletes the allocated heap memory (using vPortFree()).
    
    In Zephy, we use k_malloc() and k_free(). There are other memory schemes aswell.

--- Soln04_Zephyr/src/main.cpp
+++ Soln04_Zephyr/src/main.cpp
@@ -3,20 +3,19 @@
 #include <iostream>
 #include <string>
 
-#include "uartpolling.h"
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
-#include "led.h"
+#include "uartpolling.h"
 
 #define DELAY1 (200U)
 #define DELAY2 (500U)
 #define UART_DELAY (100U)
 // Threads
-#if CONFIG_BOARD_ESP_WROVER_KIT
-constexpr const size_t kThreadStackSize = 4 * 1024;
+#if CONFIG_BOARD_ESP
+constexpr size_t kThreadStackSize = 4 * 1024;
 #else
-constexpr const size_t kThreadStackSize = 2 * 1024;
+constexpr size_t kThreadStackSize = 2 * 1024;
 #endif
 
 static k_thread thread_0;
@@ -28,38 +27,43 @@ static k_tid_t thread_1_tid;
 K_THREAD_STACK_DEFINE(stack_thread_0, kThreadStackSize);
 K_THREAD_STACK_DEFINE(stack_thread_1, kThreadStackSize);
 
-constexpr const int thread_0_prio = 1;
-constexpr const int thread_1_prio = 1;
-
-// In the corresponding DT overlays, we defined same gpio pin with same alias
-constexpr const gpio_dt_spec led_pin =
-    GPIO_DT_SPEC_GET(DT_ALIAS(userled0), gpios);
+constexpr int thread_0_prio = 1;
+constexpr int thread_1_prio = 1;
 
 constexpr const device *uart_port = (DEVICE_DT_GET(DT_ALIAS(usercom1)));
 
-static std::atomic<uint32_t> led_delay = DELAY2;
-
-Led led{led_pin};
+// Led led{led_pin};
 UartPolling user_com_port{uart_port};
 
-static void led_toggle_thread(void *param1, void *param2, void *param3) {
+// Shared buffer:
+using shared_ptr = char *;
+static std::atomic<shared_ptr> shared_data_ptr = NULL;
 
+static void uart_write_thread(void *param1, void *param2, void *param3) {
+
+  std::string prompt_string = "\n\rYou entered:\n\r";
   while (true) {
-    // std::cout << "led_toggle_thread @" << led_delay.load() << "ms" <<
-    // std::endl;
-    if (led.Toggle()) {
-      std::cout << "Thread "
-                << ": led toggle error" << std::endl;
+    shared_ptr temp = shared_data_ptr.load();
+
+    if (temp != NULL) {
+      std::string print_string = prompt_string + std::string(temp) + "\n\r";
+      char *print = print_string.data();
+      while (*print != '\0') {
+        user_com_port.Write(static_cast<unsigned char>(*print++));
+      }
+      k_free(temp);
+      temp = NULL;
+      shared_data_ptr.store(temp);
     }
-    k_msleep(led_delay.load());
+    k_msleep(UART_DELAY);
   }
 }
 
 static void uart_read_thread(void *param1, void *param2, void *param3) {
   int ret = 0;
   size_t index = 0;
-  unsigned char read_buff[100] = {'0'};
-  size_t read_buff_size = sizeof(read_buff);
+  size_t read_buff_size = 100;
+  unsigned char read_buff[read_buff_size] = {'0'};
 
   while (true) {
 
@@ -71,29 +75,21 @@ static void uart_read_thread(void *param1, void *param2, void *param3) {
           break;
         }
       }
-      k_msleep(3 * UART_DELAY);
+      //k_msleep(2.5 * UART_DELAY);
     } while (!ret);
 
     read_buff[index] = '\0';
 
-    if (index) {
-      sscanf((const char *)read_buff, "%[0-9]s", read_buff);
-
-      if ((atoi((const char *)read_buff))) {
-        k_thread_suspend(
-            thread_1_tid); // Immediate effect: Suspend the led task as it is
-                           // currently blocked due to k_msleep()
-        led_delay.store((atoi((const char *)read_buff)));
-        std::cout << "\n\rUpdate delay time to: "
-                  << atoi((const char *)read_buff) << "ms" << std::endl;
-        k_thread_resume(thread_1_tid); // Immediate effect: Start blinking
-        // with
-        //  new delay time
-      } else {
-        std::cout << "\n\rEnter only numbers[0-9], No delay time update!"
-                  << std::endl;
+    if (read_buff[index - 1] == '\n' || read_buff[index - 1] == '\r') {
+      shared_ptr temp = shared_data_ptr.load();
+      if (temp == NULL) {
+        temp = (char *)k_malloc((index + 1) * sizeof(char));
+        memcpy(temp, read_buff, index + 1);
+        shared_data_ptr.store(temp);
+        index = 0;
       }
-      memset(read_buff, 0, index); // Clear the buffer
+    } else if(index > read_buff_size - 2) {
+      std::cout << "uart read buffer overflow, resetting..." << std::endl;
       index = 0;
     }
     k_msleep(UART_DELAY);
@@ -101,10 +97,6 @@ static void uart_read_thread(void *param1, void *param2, void *param3) {
 }
 
 extern "C" int main(void) {
-
-  if (led.Init()) {
-    return 0;
-  }
 
   if (!user_com_port.Init()) {
     std::cout << "uart config failed ..." << std::endl;
@@ -116,17 +108,17 @@ extern "C" int main(void) {
     return 0;
   }
 
-  std::cout << "Starting uart Thread ..." << std::endl;
+  std::cout << "Starting uart read Thread ..." << std::endl;
 
   thread_0_tid = k_thread_create(
       &thread_0, stack_thread_0, K_THREAD_STACK_SIZEOF(stack_thread_0),
       uart_read_thread, NULL, NULL, NULL, thread_0_prio, K_USER, K_NO_WAIT);
 
-  std::cout << "Starting LED Thread ..." << std::endl;
+  std::cout << "Starting uart write Thread ..." << std::endl;
 
   thread_1_tid = k_thread_create(
       &thread_1, stack_thread_1, K_THREAD_STACK_SIZEOF(stack_thread_1),
-      led_toggle_thread, NULL, NULL, NULL, thread_1_prio, K_USER, K_NO_WAIT);
+      uart_write_thread, NULL, NULL, NULL, thread_1_prio, K_USER, K_NO_WAIT);
 
   while (true) {
     // Do nothing
