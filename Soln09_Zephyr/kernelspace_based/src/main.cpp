d9ab02016ffa16ae95560bbc76052f27c67677fa
Akshay Narahari Kulkarni <akshaynkulkarni@gmail.com>
2023-07-16 23:18:02 +0200

    Soln9_Zephyr: Hardware Interrupt Challenge (ADC driver TBD) This patch is inspired from challenge at  www.digiKey.in (https://www.digikey.in/en/maker/projects/introduction-to- rtos-solution-to-part-9-hardware-interrupts/3ae7a68462584e1eb408e1638002e9ed)
    Challenge:
    Your challenge is to create a sampling, processing, and interface system
    using hardware interrupts and whatever kernel objects (e.g. queues,
    mutexes, and semaphores) you might need.
    
    You should implement a hardware timer in the ESP32 that samples from
    an ADC pin once every 100 ms. This sampled data should be copied to
    a double buffer (you could also use a circular buffer).
    Whenever one of the buffers is full, the ISR should notify Task A.
    
    Task A, when it receives notification from the ISR, should wake up
    and compute the average of the previously collected 10 samples.
    Note that during this calculation time,the ISR may trigger again.
    This is where a double (or circular) buffer will help: you can process
    one buffer while the other is filling up.
    
    When Task A is finished, it should update a global floating point
    variable that contains the newly computed average. Do not assume
    that writing to this floating point variable will take a single
    instruction cycle! You will need to protect that action as we
    saw in the queue episode.
    
    Task B should echo any characters received over the serial port
    back to the same serial port. If the command “avg” is entered,
    it should display whatever is in the global average variable.
    
    This is like a “final project” in that you will need to use
    many of the concepts we covered in previous lectures and challenges.
    
    TBD: ADC driver in DT and usage in app

---
+++ Soln09_Zephyr/kernelspace_based/src/main.cpp
@@ -0,0 +1,267 @@
+#include <atomic>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include "uartpolling.h"
+
+#define LED_DELAY_DEF (500U)
+#define UART_DELAY (100U)
+
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
+
+// devices
+constexpr const device *uart_port = (DEVICE_DT_GET(DT_ALIAS(usercom0)));
+
+UartPolling user_com_port{uart_port};
+
+// Threads
+#if CONFIG_BOARD_ESP
+constexpr size_t kThreadStackSize = 4 * 1024;
+#else
+constexpr size_t kThreadStackSize = 2 * 1024;
+#endif
+
+static k_thread thread_0;
+static k_thread thread_1;
+
+static k_tid_t thread_0_tid;
+static k_tid_t thread_1_tid;
+
+K_THREAD_STACK_DEFINE(stack_thread_0, kThreadStackSize);
+K_THREAD_STACK_DEFINE(stack_thread_1, kThreadStackSize);
+
+constexpr int thread_0_prio = 10;
+constexpr int thread_1_prio = 10;
+
+// circular buffer
+
+constexpr size_t buffer_len =
+    4; // The ring buffer has 'n' buffers that can be configured. Min. 2
+constexpr size_t buffer_mem_len = 10;
+
+using buf = struct {
+  uint16_t adc_val[buffer_mem_len];
+};
+
+volatile buf ring_buffer[buffer_len] = {0};
+volatile uint8_t head = 0;
+volatile uint8_t tail = 0;
+
+std::atomic<float> avg_adc = {0.0f};
+
+// syncs
+k_sem signal_buff_full; // Signal a buffer is full
+k_mutex avg_mutex;      // sync between the adc processor task and uart task
+
+// Timer stuff
+
+k_timer adc_read_timer;
+
+void adc_read_work_handler(struct k_work *work);
+void adc_read_timer_expiry_handler(k_timer *id);
+
+k_work adc_read_work = {
+    .handler = adc_read_work_handler,
+};
+
+#define DBG 0
+void adc_read_work_handler(struct k_work *work) {
+  if (((head + 1) % buffer_len) == tail) {
+    // drop the elements, buffer full, sorry
+    LOG_INF("ISR: Sorry! Buffer Full!! dropping adc_values....");
+    return;
+  }
+
+  static size_t buffer_mem_count = 0;
+
+  if (buffer_mem_count < buffer_mem_len) {
+    size_t l_buffer_mem_count = buffer_mem_count;
+
+    ring_buffer[head].adc_val[buffer_mem_count] =
+        buffer_mem_count; // adc1_get_raw(ADC1_CHANNEL_5); FIXME:...
+    buffer_mem_count++;
+
+#if DBG
+    LOG_INF("ISR ring_buffer[%d].adc_val[%d] = %d", head, l_buffer_mem_count,
+            ring_buffer[head].adc_val[l_buffer_mem_count]);
+#endif
+  }
+
+  if (buffer_mem_count >= buffer_mem_len) {
+
+    head = ((head + 1) % buffer_len); // let's fill the next buffer element
+    buffer_mem_count = 0;             // reset the member buffer count
+
+    // Signal a buffer is ready to be consumed
+    k_sem_give(&signal_buff_full);
+  }
+}
+
+void adc_read_timer_expiry_handler(k_timer *id) {
+  // handle timer expiry
+  // k_work_submit(&adc_read_work);
+
+  if (((head + 1) % buffer_len) == tail) {
+    // drop the elements, buffer full, sorry
+    LOG_INF("ISR: Sorry! Buffer Full!! dropping adc_values....");
+    return;
+  }
+
+  static size_t buffer_mem_count = 0;
+
+  if (buffer_mem_count < buffer_mem_len) {
+    size_t l_buffer_mem_count = buffer_mem_count;
+
+    ring_buffer[head].adc_val[buffer_mem_count] =
+        buffer_mem_count; // adc1_get_raw(ADC1_CHANNEL_5); FIXME:...
+    buffer_mem_count++;
+
+#if DBG
+    LOG_INF("ISR ring_buffer[%d].adc_val[%d] = %d", head, l_buffer_mem_count,
+            ring_buffer[head].adc_val[l_buffer_mem_count]);
+#endif
+  }
+
+  if (buffer_mem_count >= buffer_mem_len) {
+
+    head = ((head + 1) % buffer_len); // let's fill the next buffer element
+    buffer_mem_count = 0;             // reset the member buffer count
+
+    // Signal a buffer is ready to be consumed
+    k_sem_give(&signal_buff_full);
+  }
+}
+
+static void adc_processing_thread(void *param1, void *param2, void *param3) {
+
+  uint32_t adc_sum = 0;
+  uint32_t l_tail = 0;
+
+  while (true) {
+    if (!k_sem_take(&signal_buff_full, K_FOREVER)) {
+      adc_sum = 0;
+
+      for (uint8_t i = 0; i < buffer_mem_len; i++) {
+        adc_sum += ring_buffer[tail].adc_val[i];
+
+        // LOG_INF("adc: ring_buffer[%d].adc_val[%d] = %d", tail, i,
+        // ring_buffer[tail].adc_val[i]);
+      }
+      l_tail = tail;
+      tail = ((tail + 1) % buffer_len);
+
+#if DBG
+      for (uint8_t i = 0; i < buffer_mem_len; i++) {
+        LOG_INF("adc: ring_buffer[%d].adc_val[%d] = %d", l_tail, i,
+                ring_buffer[l_tail].adc_val[i]);
+      }
+      LOG_INF("=========", "==========");
+#endif
+      if (!k_mutex_lock(&avg_mutex, K_FOREVER)) {
+        avg_adc.store((float)(((float)adc_sum / (buffer_mem_len))));
+        k_mutex_unlock(&avg_mutex);
+      }
+    }
+    // k_msleep(10 * UART_DELAY); // only to observe buffer full
+  }
+}
+
+static void uart_read_thread(void *param1, void *param2, void *param3) {
+  int ret = 0;
+  size_t index = 0;
+  size_t read_buff_size = 100;
+  unsigned char read_buff[read_buff_size] = {'0'};
+
+  while (true) {
+
+    do {
+      while (!(ret = user_com_port.Read(read_buff[index]))) {
+        user_com_port.Write(read_buff[index++]);
+
+        if (index > read_buff_size - 1) {
+          index = read_buff_size - 1;
+          ret = 1;
+          break;
+        }
+        k_msleep(3 * UART_DELAY);
+      }
+    } while (!ret);
+
+    read_buff[index] = '\0';
+
+    if (index) {
+
+      if (!strcmp("avg\r", (const char *)read_buff) ||
+          !strcmp("avg\n", (const char *)read_buff)) {
+        float l_avg = 0.0f;
+        if (!k_mutex_lock(&avg_mutex, K_FOREVER)) {
+          l_avg = avg_adc.load();
+          k_mutex_unlock(&avg_mutex);
+        }
+        LOG_INF("Average is %f, Voltage at Pin = %u mV", l_avg, 0);
+        // l_avg, esp_adc_cal_raw_to_voltage(l_avg, &adc1_chars));
+      }
+    }
+
+    memset(read_buff, 0, index + 1); // clear the local buffer
+    index = 0;
+    k_msleep(UART_DELAY);
+  }
+}
+
+extern "C" int main(void) {
+
+  // TBD: ADC init
+  //  ----
+
+  if (k_mutex_init(&avg_mutex)) {
+    LOG_ERR("mutex avg_mutex init failed");
+    return 0;
+  }
+
+  if (k_sem_init(&signal_buff_full, 0, buffer_len)) {
+    LOG_ERR("semaphore signal_buff_full init failed");
+    return 0;
+  }
+
+  if (!user_com_port.Init()) {
+    LOG_ERR("uart config failed ...");
+    return 0;
+  }
+
+  if (!user_com_port.IsReady()) {
+    LOG_ERR("uart port not found...");
+    return 0;
+  }
+
+  LOG_INF("Starting uart Thread ...");
+
+  thread_0_tid = k_thread_create(
+      &thread_0, stack_thread_0, K_THREAD_STACK_SIZEOF(stack_thread_0),
+      uart_read_thread, NULL, NULL, NULL, thread_0_prio, 0, K_NO_WAIT);
+
+  LOG_INF("Starting adc processing Thread ...");
+
+  thread_1_tid = k_thread_create(
+      &thread_1, stack_thread_1, K_THREAD_STACK_SIZEOF(stack_thread_1),
+      adc_processing_thread, NULL, NULL, NULL, thread_1_prio, 0, K_NO_WAIT);
+
+  k_timer_init(&adc_read_timer, adc_read_timer_expiry_handler, NULL);
+  LOG_INF("Starting ADC Timer ...");
+
+  k_timer_start(&adc_read_timer, K_MSEC(100), K_MSEC(100));
+  while (true) {
+    // Do nothing
+    k_msleep(2 * LED_DELAY_DEF);
+  }
+
+  return 0;
+}
